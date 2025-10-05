655d955bdc806dbcdce30a47c0dcbeee5f498652
ChathurK <kumaras-im21001@stu.kln.ac.lk>
2025-10-05 10:39:36 +0530

    Issues fixed: Add material window height, Upload date column removed, add UserDTO

--- backend/OnlineBookStore/src/main/java/com/project/OnlineBookStore/dto/PurchaseDTO.java
+++ backend/OnlineBookStore/src/main/java/com/project/OnlineBookStore/dto/PurchaseDTO.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Setter
 public class PurchaseDTO {
     private Long id;
+    private UserDTO user;
     private MaterialDTO material;
     private Double purchasePrice;
     private LocalDateTime purchaseDate;
--- backend/OnlineBookStore/src/main/java/com/project/OnlineBookStore/service/DTOConversionService.java
+++ backend/OnlineBookStore/src/main/java/com/project/OnlineBookStore/service/DTOConversionService.java
@@ -2,8 +2,11 @@ package com.project.OnlineBookStore.service;
 
 import com.project.OnlineBookStore.dto.MaterialDTO;
 import com.project.OnlineBookStore.dto.PurchaseDTO;
+import com.project.OnlineBookStore.dto.UserDTO;
 import com.project.OnlineBookStore.model.Material;
 import com.project.OnlineBookStore.model.Purchase;
+import com.project.OnlineBookStore.model.User;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,9 +26,19 @@ public class DTOConversionService {
         return dto;
     }
 
+    public UserDTO convUserDTO(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setFullName(user.getFullName());
+        dto.setEmail(user.getEmail());
+        dto.setRole(user.getRole().name());
+        return dto;
+    }
+
     public PurchaseDTO convertToPurchaseDTO(Purchase purchase) {
         PurchaseDTO dto = new PurchaseDTO();
         dto.setId(purchase.getId());
+        dto.setUser(convUserDTO(purchase.getUser()));
         dto.setMaterial(convertToMaterialDTO(purchase.getMaterial(), true));
         dto.setPurchasePrice(purchase.getPurchasePrice());
         dto.setPurchaseDate(purchase.getPurchaseDate());
